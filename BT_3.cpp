84c77df2a132794ca6e557c4acaf888c7b8b8ed2
dngtundt <dtun11111@gmail.com>
2024-07-31 16:54:22 +0700

    finished BT_3

---
+++ BT_3.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+double tinhTongDeQuy(int n) {
+    if (n == 1) {
+        return 1.0 / 2;
+    }
+    return (double)n / (n + 1) + tinhTongDeQuy(n - 1);
+}
+
+int main() {
+    int n;
+
+    printf("Nhap so nguyen n: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Vui long nhap so nguyen duong.\n");
+        return 1;
+    }
+
+    double tong = tinhTongDeQuy(n);
+    printf("Tong S =  %.6f\n", tong);
+
+    return 0;
+}
